2fb597bc2b19e5255dbbc2f6a50b97c3047eda18
DHIRAJ PAWAR <160473682+dhi2468@users.noreply.github.com>
2024-05-30 00:21:08 +0530

    Add files via upload

---
+++ JS THEORY/THEORY.PRO/12.js
@@ -0,0 +1,14 @@
+
+// How can you convert the string of any base to an integer in JavaScript?
+
+const binaryString = "101010";
+const binaryInteger = parseInt(binaryString, 2);
+console.log(binaryInteger);
+
+const octalString = "52";
+const octalInteger = parseInt(octalString, 8);
+console.log(octalInteger);
+
+const hexString = "2A";
+const hexInteger = parseInt(hexString, 16);
+console.log(hexInteger);
